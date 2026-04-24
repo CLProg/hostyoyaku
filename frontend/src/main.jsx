e7bc3b2d9ce362143859dd08da0936f28fcfe64b
CLProg <carllorenz32@gmail.com>
2026-04-25 01:37:19 +0800

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import App from "./App.jsx";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// --- ADD THIS SECTION ---
+// This ensures all axios calls use your Render URL automatically
+axios.defaults.baseURL =
+  import.meta.env.VITE_APP_URL || "http://localhost:5000";
+axios.defaults.withCredentials = true;
+// ------------------------
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
-
+);
